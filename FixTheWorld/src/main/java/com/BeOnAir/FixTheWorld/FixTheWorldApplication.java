8b1a0a64acb4f6239d7c5ef866e6eda87eb8cb7b
OnerBerk <oner.berk@joza-it.fr>
2022-09-06 15:42:43 +0200

    creation du projet de sauvetage du monde

---
+++ FixTheWorld/src/main/java/com/BeOnAir/FixTheWorld/FixTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.BeOnAir.FixTheWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixTheWorldApplication.class, args);
+	}
+
+}
---
+++ FixTheWorld/src/test/java/com/BeOnAir/FixTheWorld/FixTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeOnAir.FixTheWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
