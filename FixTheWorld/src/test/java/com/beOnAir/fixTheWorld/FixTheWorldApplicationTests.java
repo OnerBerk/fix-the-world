691d5cd3a3e42aca7a9762b3484725c3bc122291
OnerBerk <oner.berk@joza-it.fr>
2022-10-02 19:41:17 +0200

    wip

---
+++ FixTheWorld/src/test/java/com/beOnAir/fixTheWorld/FixTheWorldApplicationTests.java
@@ -0,0 +1,12 @@
+package com.beOnAir.fixTheWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixTheWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
